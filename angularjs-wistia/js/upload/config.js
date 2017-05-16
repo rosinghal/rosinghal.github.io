0e375f497f97ef52777b8ed8374e18e1af1ebda2
Rohit Singhal <rohitsinghal.rs@gmail.com>
2017-05-16 20:06:46 +0530

    delete and list

--- angularjs-wistia/js/upload/config.js
+++ angularjs-wistia/js/upload/config.js
@@ -14,7 +14,11 @@
     vm.files = {};
     vm.progress = 0;
     vm.checkStatus = checkStatus;
+    vm.listAll = listAll;
+    vm.deleteMedia = deleteMedia;
     vm.wistiaApiPassword = "b32a71acd5b3a1bab3447ac253eaa80d5c8bfaf3ba1235bb788f6bf56a24790f";
+
+    vm.listAll();
 
     $('#fileupload').fileupload({
       url: 'https://upload.wistia.com',
@@ -29,12 +33,7 @@
       done: function (e, data) {
         if (data.result.hashed_id) {
           var hashed_id = data.result.hashed_id;
-          vm.files[hashed_id] = {
-            // url: null,
-            name: data.result.name,
-            hashed_id: hashed_id,
-            status: data.result.status
-          };
+          handleFile(data.result);
           vm.checkStatus(hashed_id);
         }
       },
@@ -47,21 +46,52 @@
       }
     });
 
+    function handleFile(file) {
+      vm.files[file.hashed_id] = {
+        // url: null,
+        name: file.name,
+        hashed_id: file.hashed_id,
+        status: file.status
+      };
+      if (file.status === 'ready') {
+        vm.files[file.hashed_id].embedCode = $sce.trustAsHtml(file.embedCode);
+        // file.url = $sce.trustAsResourceUrl('http://fast.wistia.net/embed/iframe/' + hashed_id);
+      } else if (file.status !== 'failed') {
+        $timeout(function(){
+          vm.checkStatus(file.hashed_id);
+        }, 2000);
+      }
+    }
+
+    function deleteMedia(hashed_id) {
+      if(vm.files[hashed_id]) {
+        $http({
+          method: 'DELETE',
+          url: 'https://api.wistia.com/v1/medias/' + hashed_id + '.json?api_password=' + vm.wistiaApiPassword
+        }).then(function (response) {
+          delete vm.files[hashed_id];
+        });
+      }
+    }
+
+    function listAll() {
+      $http({
+        method: 'GET',
+        url: 'https://api.wistia.com/v1/medias.json?api_password=' + vm.wistiaApiPassword
+      }).then(function (response) {
+        console.log(response);
+        response.data.forEach(function (file) {
+          handleFile(file);
+        });
+      });
+    }
+
     function checkStatus(hashed_id) {
-      var file = vm.files[hashed_id];
       $http({
         method: 'GET',
         url: 'https://api.wistia.com/v1/medias/' + hashed_id + '.json?api_password=' + vm.wistiaApiPassword
       }).then(function (response) {
-        file.status = response.data.status || '';
-        if (file.status === 'ready') {
-          file.embedCode = $sce.trustAsHtml(response.data.embedCode);
-          // file.url = $sce.trustAsResourceUrl('http://fast.wistia.net/embed/iframe/' + hashed_id);
-        } else if (vm.files[hashed_id].status !== 'failed') {
-          $timeout(function(){
-            vm.checkStatus(hashed_id);
-          }, 2000);
-        }
+        handleFile(response.data);
       });
     }
   }
